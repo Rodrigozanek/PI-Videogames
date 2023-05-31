caa6fd120b3e75101bd63800e9627eaa723081b1
rodrigozanek <rodrigozanek22@gmail.com>
2023-05-30 22:50:33 -0300

    back en proceso

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,33 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image: {
+      type: DataTypes.TEXT,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false
+ });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const genres = require('./genres')
+const videogame = require('./videogame')
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/genres', genres);
+router.use('/videogame', videogame);
 
 
 module.exports = router;
